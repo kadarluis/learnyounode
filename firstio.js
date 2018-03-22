d050383804ae1de0b0743a0a99233895ee623337
kadarluis <kadarluisg@gmail.com>
2018-03-21 22:49:13 -0500

    Ejercicio 2

--- firstio.js
+++ firstio.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var saltos = fs.readFileSync(process.argv[2])
-                .toString()
+                .toString() 
                 .split('\n')
                 .length-1;
 console.log(saltos);
