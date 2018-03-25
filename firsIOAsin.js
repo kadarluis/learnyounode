44e51a8e5dfd0cdd22c64b165fde298939724c8e
kadarluis <kadarluisg@gmail.com>
2018-03-24 23:44:21 -0500

    Ejercicio 6

--- firsIOAsin.js
+++ firsIOAsin.js
@@ -3,5 +3,5 @@ fs.readFile(process.argv[2], 'utf8', function (err, data){
     if (err) throw err;
     var saltos = data.split('\n').length-1;
     console.log(saltos);
-    
+        
 });
--- lsFiltrado.js
+++ lsFiltrado.js
@@ -3,8 +3,9 @@ var path = require ('path');
 var ruta = process.argv[2];
 
 fs.readdir(ruta, function (err, data) {
+   if (err) throw err;
     data.forEach(function(data) {
-        ext = path.extname(data);
+        var ext = path.extname(data);
         if ("." + process.argv[3] === ext) {
             console.log(data);
         }                
